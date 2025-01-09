1a8235ac15caeebcfe8da7b3f8e50cea9b701e70
zzq1234567890 <zhangzhenqiu@foxmail.com>
2025-01-09 12:49:32 +0800

    Update youtubelivetxt.php

--- youtubelivetxt.php
+++ youtubelivetxt.php
@@ -1,39 +1,8 @@
 <?php
 //$id=$_GET['id'];
 header( 'Content-Type: text/plain; charset=UTF-8');
-//header( 'Content-Type: html/text; charset=utf-8');
-//ini_set("max_execution_time", "900000000");
-//htaccess php_value max_execution_time 0;
-//ini_set('date.timezone','Asia/Shanghai');
-
-
 $fp="youtubelive.txt";//压缩版本的扩展名后加.gz
-//ini_set("max_execution_time", "3000000");
 $chn="\n";
-
-//header("Content-Disposition: attachment; filenameyoutubelive.txt");
-function escape($str) 
-{ 
-preg_match_all("/[\x80-\xff].|[\x01-\x7f]+/",$str,$r); 
-$ar = $r[0]; 
-foreach($ar as $k=>$v) 
-{ 
-if(ord($v[0]) < 128) 
-$ar[$k] = rawurlencode($v); 
-else 
-$ar[$k] = "%u".bin2hex(iconv("UTF-8","UCS-2",$v)); 
-} 
-return join("",$ar); 
-} 
-//適合php7以上
-function replace_unicode_escape_sequence($match)
-{       
-		return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UCS-2BE');     
-}          
-
-
-
-
 $chn.=  "遊戲直播,#genre#\r\n";
 $url2='https://www.youtube.com/playlist?list=PLiCvVJzBupKlQ50jZqLas7SAztTMEYv1f';//遊戲
 $ch2=curl_init();
